4c34aaf9d2ea9e4d2f79e88dfa997a80a1029a3a
anasu <misushima@gmail.com>
2020-03-17 22:15:39 -0300

    Se imprimen datos de nombre y ID de pokemon

--- js/pokeData.js
+++ js/pokeData.js
@@ -1,5 +1,21 @@
 let pokeData = {
     template: `
-        <h2 class="respuesta__titulo">Concatenar ID y Nombre</h2>
+        <h2 class="respuesta__nombre">#{{ pokeID }} - {{ pokeName }}</h2>
     `,
+    props: {
+        pokeName: {
+            type: String,
+            required: false,
+            default() {
+                return 'No se encontró el pokemon';
+            }
+        },
+        pokeID: {
+            type: Number,
+            required: false,
+            default() {
+                return 0;
+            }
+        }
+    }
 }
