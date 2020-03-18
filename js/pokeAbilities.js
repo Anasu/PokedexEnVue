48d228a87d318d471dbe993f31d9f648b4fe28ec
anasu <misushima@gmail.com>
2020-03-17 22:17:36 -0300

    Se logran enviar datos a pokeabilities (pero aun falta imprimirlos en pantalla)

--- js/pokeAbilities.js
+++ js/pokeAbilities.js
@@ -2,10 +2,23 @@ let pokeAbilities = {
     template: `
         <div class="respuesta__habilidades_centrado">
             <!-- usar un v-for -->
-            <div class="habilidad__texto">\
-                <h3>Titulo de Habilidad</h3>
-                <p>Descripcion en español</p>
+            <div class="habilidad__texto">
+                <h3>{{ pokeAbility.name }}</h3>
+                <p>{{ pokeAbility.url }}</p>
             </div>
         </div>
     `,
+    props: {
+        pokeAbility: {
+            type: Object,
+            required: false,
+            default() {
+                return {
+                    'name': 'Habilidad no encontrada',
+                    'url': 'URL no encontrada'
+                }
+            }
+        }
+    },
+
 }
