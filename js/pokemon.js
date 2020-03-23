93162790d74df8a6035e2a1a2484903865ee1156
anasu <misushima@gmail.com>
2020-03-23 11:27:43 -0300

    Se corrige comportamiento en fail

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,26 +1,28 @@
 let pokemon = {
     template: `
-        <div class="respuesta">
+        <div class="respuesta" >
             <div class="pokemon__card">
                 <div class="respuesta__titulo">
                     <poke-image :pokeImgUrl="objRqstd.id"></poke-image>
                     <poke-data :pokeName="pokemonName" :pokeID="pokemonID"></poke-data>
                 </div>
-                <div class="respuesta__habilidades">
-                    <h2>Habilidades</h2>
-                    <poke-abilities 
-                        v-for="ability in abilities"
-                        :pokeAbility="ability.ability"
-                        :key="ability.name">
-                    </poke-abilities>
-                </div>
-                <div class="respuesta__movimientos">
-                    <h2>Movimientos</h2>
-                    <poke-moves
-                        v-for="move in moves"
-                        :pokeMove="move.move"
-                        :key="move.name">
-                    </poke-moves>
+                <div v-if="status == 'ok'">
+                    <div class="respuesta__habilidades">
+                        <h2>Habilidades</h2>
+                        <poke-abilities 
+                            v-for="ability in abilities"
+                            :pokeAbility="ability.ability"
+                            :key="ability.name">
+                        </poke-abilities>
+                    </div>
+                    <div class="respuesta__movimientos">
+                        <h2>Movimientos</h2>
+                        <poke-moves
+                            v-for="move in moves"
+                            :pokeMove="move.move"
+                            :key="move.name">
+                        </poke-moves>
+                    </div>
                 </div>
             </div>
         </div>
@@ -46,8 +48,18 @@ let pokemon = {
             default() {
                 return {};
             }
+        },
+        status: {
+            type: String,
+            required: true,
+            default() {
+                return 'hidden';
+            }
         }
     }, 
+    created() {
+
+    },
     updated() {
         this.abilities = this.objRqstd.abilities;
         this.moves = this.objRqstd.moves;
